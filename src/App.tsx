91e27863f028262f6ec4446f512cbd725ce39322
wmc15156 <wmc15156@naver.com>
2021-12-22 05:23:27 +0900

    console 삭제
    --- COMMIT END ---

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { RecoilRoot, useRecoilState } from 'recoil';
-import moment from 'moment';
-import momentTimeZone from 'moment-timezone';
 
 import { PublicRouter } from './common/router';
 import { TimeZoneAtom } from './store/atoms';
@@ -14,18 +12,6 @@ function App() {
     <RecoilRoot>
       <PublicRouter />
     </RecoilRoot>
-    // <main>
-    //   <div id="contents">
-    //     <HomeHeader />
-    //     <HomeSectionOne />
-    //     <HomeSectionTwo />
-    //     <HomeSectionThree />
-    //     <HomeSectionFour />
-    //     <HomeSectionFive />
-    //     <HomeSectionSix />
-    //     <HomeFooter />
-    //   </div>
-    // </main>
   );
 }
 
--- src/Components/atoms/CountDown/index.tsx
+++ src/Components/atoms/CountDown/index.tsx
@@ -1,26 +1,16 @@
+import React from 'react';
 import ReactCountDown, { zeroPad } from 'react-countdown';
-import React, { useEffect } from 'react';
 import moment from 'moment';
-import { useRecoilState } from 'recoil';
-import { TimeZoneAtom } from '../../../store/atoms';
 import momentTimeZone from 'moment-timezone';
 
 const CountDown = () => {
   const dDay = '2021-12-31 12:12:12';
 
   const CompletedTime = () => <span>D-DAY</span>;
-  const [timeZoneAtom, setTimeZoneAtom] = useRecoilState(TimeZoneAtom);
-  const gmt = moment().utc();
   const krTime = momentTimeZone(dDay).tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
 
   const localTime = moment(dDay).format('YYYY-MM-DD HH:mm:ss');
   const differ = moment(krTime).valueOf() - moment(localTime).valueOf();
-
-  useEffect(() => {
-    const date = moment().valueOf() + 864000000;
-    setTimeZoneAtom(date);
-  }, []);
-
   const renderer = ({
     days,
     hours,
